af4516e683916f939e21571a098d287b76d63053
Steve Dignam <steve@dignam.xyz>
2015-12-17 21:41:10 -0500

    fixed some errors... & updated formula to latest version

--- macchanger.rb
+++ macchanger.rb
@@ -1,14 +1,15 @@
 class Macchanger < Formula
   desc "Change a network device's MAC address with ease"
-  homepage 'https://github.com/sbdchd/macchanger'
-  url 'https://github.com/sbdchd/macchanger/archive/v0.1.tar.gz'
-  sha256 'd7a91ba0974a8533585fc4b3274d9754d7a4a85b5033960276275569c4c89206'
+  homepage "https://github.com/sbdchd/macchanger"
+  url "https://github.com/sbdchd/macchanger/archive/v1.0.tar.gz"
+  sha256 "73c49327d759572cb5d6e13e4ba9f82b746a28c56403c0c111d4b7d433bd78ce"
 
-  install do
-    bin.install 'macchanger'
+  def install
+    bin.install "macchanger"
+    man1.install "macchanger.1"
   end
 
   test do
-    system "#{bin}/macchanger"
+    system "#{bin}/macchanger_test"
   end
 end
